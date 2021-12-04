4496cba15d5e510f5ba4adbecd02312045202da3
Filip Rokita <filiprokita4@gmail.com>
2021-12-05 00:03:54 +0100

    push

---
+++ FR_Spammer/FR_Spammer.cpp
@@ -0,0 +1,47 @@
+﻿//FR_Spammer
+//Filip Rokita
+//filiprokita.com
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+string tempname, ffn;
+int number;
+
+int main()
+{
+	for (int i = 0; i <= 999999; i++)
+	{
+		number = i;
+		ostringstream convert;
+		convert << number;
+		tempname = convert.str();
+		cout << tempname << endl;
+
+
+
+		fstream tempsave;
+		tempsave.open("FR_Temp.txt", ios::out);
+		tempsave << "FR_" << tempname << ".txt";
+		tempsave.close();
+
+
+
+		fstream tempopen;
+		tempopen.open("FR_Temp.txt", ios::in);
+		if (tempopen.good() == false) exit(0);
+		getline(tempopen, ffn);
+		tempopen.close();
+
+
+
+		fstream f;
+		f.open(ffn, ios::out);
+		f << "filiprokita.com" << endl;
+		f.close();
+	}
+	return 0;
+}
